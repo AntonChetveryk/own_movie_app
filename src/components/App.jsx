a366457d14fbba910b0988c8b322a6f6c527cca4
AntonChetveryk <chetverykanton92@gmail.com>
2020-04-28 22:11:48 +0300

    start hw filters

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,35 @@ import Filters from "./Filters/Filters";
 import MoviesList from "./Movies/MoviesList";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filters: {
+        sort_by: "popularity.desc",
+      },
+      page: 1,
+    };
+  }
+
+  onChangeFilters = (event) => {
+    const { value, name } = event.target;
+    this.setState((prevState) => ({
+      filters: {
+        ...prevState.filters,
+        [name]: value,
+      },
+    }));
+  };
+
+  onChangePage = (page) => {
+    this.setState({
+      page,
+    });
+  };
+
   render() {
+    const { filters, page } = this.state;
     return (
       <div className="container">
         <div className="row mt-4">
@@ -11,12 +39,21 @@ export default class App extends React.Component {
             <div className="card" style={{ width: "100%" }}>
               <div className="card-body">
                 <h3>Фильтры:</h3>
-                <Filters />
+                <Filters
+                  page={page}
+                  filters={filters}
+                  onChangeFilters={this.onChangeFilters}
+                  onChangePage={this.onChangePage}
+                />
               </div>
             </div>
           </div>
           <div className="col-8">
-            <MoviesList />
+            <MoviesList
+              filters={filters}
+              page={page}
+              onChangePage={this.onChangePage}
+            />
           </div>
         </div>
       </div>

c637cbc05b1cdab7f627a264147604b4fd72348f
AntonChetveryk <chetverykanton92@gmail.com>
2020-05-15 20:40:34 +0300

    add import styles.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "./stylesheets/index.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
